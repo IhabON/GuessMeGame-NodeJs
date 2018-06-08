5740363a9c97be81d003b2dc27c05a7aa4f964dc
IhabON <ihab7@hotmail.fr>
2018-06-08 23:37:48 +0200

    Gestion des room par Socket.io car difficulté avec les subcsribe et les session.

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-var socket = io();
 let isPressingCanvas = false;
 let color;
 let size = 6;
@@ -12,9 +11,6 @@ const CURRENT_CHANNEL = location.pathname.slice(1);
 // Prepare the canvas
 const canvas = document.createElement("canvas");
 const container = document.getElementById("canvas-container");
-
-//Info Form
-var nickname = document.getElementById("nickname");
 
 canvas.id = "CursorLayer";
 canvas.width = container.clientWidth;
@@ -46,12 +42,7 @@ socket.addEventListener("message", event => {
             message.payload.size
         );
     }
-
-    if(message.type == "message")
-    {
-       sendName();
-    }
-
+    if (message.type == "wordToGuess"){}
 });
 
 socket.addEventListener("open", () => {
@@ -61,17 +52,6 @@ socket.addEventListener("open", () => {
 function sendMessage(type, payload) {
     const message = { type, payload, channel: CURRENT_CHANNEL };
     socket.send(JSON.stringify(message));
-    
-}
-
-function sendName()
-{
-    
-    Namevalue = nickname.value;
-    
-    
-    socket.send(JSON.stringify(Namevalue));
-     
 }
 
 function drawInCanvas(ctx, x, y, color, size) {
@@ -130,22 +110,119 @@ const onMouseUp = () => {
 canvas.addEventListener("mousemove", onMouseMove, false);
 canvas.addEventListener("mousedown", onMouseDown, false);
 canvas.addEventListener("mouseup", onMouseUp, false);
+const $messageInput = $("#guess");
 
-  
+
+$("#valider").on("click", e => {
+    e.preventDefault();
+    sendMessage("message", { message: $messageInput.val() });
+});
+
+$("#clearcanvas").on("click", e => {clearScreen()});
 
 function clearScreen()
 {
     var context = canvas.context;
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
-    
+
+function usernameAsk() {
+    $('.grey-out').fadeIn(500);
+    $('.user').fadeIn(500);
+    $('.user').submit(function(){
+        event.preventDefault();
+        user = $('#username').val().trim();
+
+        if (user == '') {
+            return false
+        };
+
+        var index = users.indexOf(user);
+
+        if (index > -1) {
+            alert(user + ' already exists');
+            return false
+        };
+        
+        socket.emit('join', user);
+        $('.grey-out').fadeOut(300);
+        $('.user').fadeOut(300);
+        $('input.guess-input').focus();
+    });
+};
+
+var guesser = function() {
+    clearScreen();
+    click = false;
+    console.log('draw status: ' + click);
+    $('.draw').hide();
+    $('#guesses').empty();
+    console.log('You are a guesser');
+    $('#guess').show();
+    $('.guess-input').focus();
+
+    $('#guess').on('submit', function() {
+        event.preventDefault();
+        var guess = $('.guess-input').val();
+
+        if (guess == '') {
+            return false
+        };
+
+        console.log(user + "'s guess: " + guess);
+        socket.emit('guessword', {username: user, guessword: guess});
+        $('.guess-input').val('');
+    });
+};
+
+var guessword = function(data){
+    $('#guesses').text(data.username + "'s guess: " + data.guessword);
+
+    if (click == true && data.guessword == $('span.word').text() ) {
+        console.log('guesser: ' + data.username + ' draw-word: ' + $('span.word').text());
+        socket.emit('correct answer', {username: data.username, guessword: data.guessword});
+        socket.emit('swap rooms', {from: user, to: data.username});
+        click = false;
+    }
+};
+
+var drawWord = function(word) {
+    $('span.word').text(word);
+    console.log('Your word to draw is: ' + word);
+};
+
+var users = [];
+
+var userlist = function(names) {
+    users = names;
+    var html = '<p class="chatbox-header">' + 'Players' + '</p>';
+    for (var i = 0; i < names.length; i++) {
+        html += '<li>' + names[i] + '</li>';
+    };
+    $('ul').html(html);
+};
+
+var newDrawer = function() {
+    socket.emit('new drawer', user);
+    clearScreen();
+    $('#guesses').empty();
+};
+
+var correctAnswer = function(data) {
+    $('#guesses').html('<p>' + data.username + ' guessed correctly!' + '</p>');
+};
+
+var reset = function(name) {
+    clearScreen();
+    $('#guesses').empty();
+    console.log('New drawer: ' + name);
+    $('#guesses').html('<p>' + name + ' is the new drawer' + '</p>');
+};
 
 $(document).ready(function() {
 
     canvas = $('#canvas');
     context = canvas[0].getContext('2d');
-    canvas[0].width = canvas[0].offsetWidth;
-    canvas[0].height = canvas[0].offsetHeight;
 
     usernameAsk();
 
@@ -161,3 +238,8 @@ $(document).ready(function() {
     socket.on('clear screen', clearScreen);
 
 });
+
+
+
+	
+	
--- server/server.js
+++ server/server.js
@@ -1,146 +1,15 @@
-const express = require('express');
-const Redis = require("redis");
-const path = require("path");
-var socket_io = require('socket.io')
+var http = require('http');
+var express = require('express');
+var socket_io = require('socket.io');
 
+var app = express();
+app.use(express.static('public'));
 
-var users = []
-let drawer = "";
-let listeguesser = [];
-const app = express();
-const http = require("http");
-const uuidv4 = require("uuid/v4");
-const WebSocket = require("ws");
-
-let sockets = [];
-const client = Redis.createClient();
-const redisPublisher = Redis.createClient();
-const redisSubscriber = Redis.createClient();
-
-const PUBLIC_FOLDER = path.join(__dirname, "../public");
-const PORT = process.env.PORT || 5000;
-
-const socketsPerChannels /* Map<string, Set<WebSocket>> */ = new Map();
-const channelsPerSocket /* WeakMap<WebSocket, Set<string> */ = new WeakMap();
-
-// Initialize a simple http server
-const server = http.createServer(app);
+var server = http.Server(app);
 var io = socket_io(server);
 
-// Initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });    
-
-/*
- * Subscribe a socket to a specific channel.
- */
-function subscribe(socket, channel) {
-    let socketSubscribed = socketsPerChannels.get(channel) || new Set();
-    let channelSubscribed = channelsPerSocket.get(socket) || new Set();
-
-    if (socketSubscribed.size == 0) {
-        redisSubscriber.subscribe(channel);
-    }
-
-    socketSubscribed = socketSubscribed.add(socket);
-    channelSubscribed = channelSubscribed.add(channel);
-
-    socketsPerChannels.set(channel, socketSubscribed);
-    channelsPerSocket.set(socket, channelSubscribed);
-
-
-}
-
-/*
- * Unsubscribe a socket from a specific channel.
- */
-function unsubscribe(socket, channel) {
-    let socketSubscribed = socketsPerChannels.get(channel) || new Set();
-    let channelSubscribed = channelsPerSocket.get(socket) || new Set();
-
-    socketSubscribed.delete(socket);
-    channelSubscribed.delete(channel);
-
-    if (socketSubscribed.size == 0) {
-        redisSubscriber.unsubscribe(channel);
-    }
-
-    socketsPerChannels.set(channel, socketSubscribed);
-    channelsPerSocket.set(socket, channelSubscribed);
-}
-
-/*
- * Subscribe a socket from all channels.
- */
-function unsubscribeAll(socket) {
-    const channelSubscribed = channelsPerSocket.get(socket) || new Set();
-
-    channelSubscribed.forEach(channel => {
-        unsubscribe(socket, channel);
-    });
-}
-
-/*
- * Broadcast a message to all sockets connected to this server.
- */
-function broadcastToSockets(channel, data) {
-    const socketSubscribed = socketsPerChannels.get(channel) || new Set();
-
-    // redisPublisher.publish(channel, data);
-
-    socketSubscribed.forEach(client => {
-        client.send(data);
-    });
-}
-
-// Broadcast message from client
-wss.on("connection", ws => {
-    ws.on('close', () => {
-        unsubscribeAll(ws);
-    });
-
-    
-
-    ws.on("message", data => {
-        const message = JSON.parse(data);
-        console.log(message.type);
-        switch (message.type) {
-            case 'subscribe':
-                client.lrange(message.channel, 0, 100, (err, result) => {
-                    result.map(data => ws.send(data));
-                });
-                subscribe(ws, message.channel);
-                var Joueur = data.data;
-                console.log(data);
-                console.log(Joueur);
-                //lancerPartie(Joueur);
-                break;
-            default:
-                client.lpush(message.channel, data);
-                redisPublisher.publish(message.channel, data);
-                broadcastToSockets(message.channel, data);
-                break;
-        }
-    });
-});
-
-function lancerPartie(Player){
-    if(Player != null){
-        let clientName = socket.nickname;
-        users.push(clientName);
-        sockets.push(socket);
-        // Log it to the server output
-        console.log(clientName + ' joined this game.\n');
-        updateConnectedUsers();
-        if(users.length < 2)
-        {
-            PartieComplete = false;
-            console.log("il manque des joueur");
-            console.log("La partie va commencer, en attente d'autre joueur");
-        }
-        else{
-            PartieComplete = true;
-            server.emit('userlist', users);
-            var	words = [
+var users = [];
+var	words = [
                 "Pomme", "Soleil", "Lune", "Voiture", "Stylo", "Police", "Chien",
                 "Son", "Eau", "baguette", "statue", "maison", "main", "vache",
                 "loup", "chaise", "poubelle", "monde", "tete", "banane", "ecole",
@@ -151,151 +20,143 @@ function lancerPartie(Player){
                 "bois", "list", "oiseau", "body", "famille", "song", "porte", "foret", "vent", "avion", "parachute",
                 "pierre", "Captain Planet", "feu", "ours", "roi", "espace", "baleine", "epee", "plume", "pigeon"
             ];
-    
+
             function newWord() {
                 wordcount = Math.floor(Math.random() * (words.length));
                 return words[wordcount];
             };
+            
+            var wordcount;
     
-            if(users.length == 4){
-                let randomUser = Math.floor(Math.random()*users.length);                
-                console.log("La Partie va commencer");
-                console.log(users[randomUser] + ' is a drawer');
-                drawer = users[randomUser];
-                if(drawer != null){
-                    console.log(users + ' are a playing');
-                    listeguesser = users;
-                    listeguesser.splice(randomUser,1);                 
-    
-                    // server submits the 'drawer' event to this user
-                    // send the random word to the user inside the 'drawer' room
-                    console.log(drawer + "'s draw word : " + newWord());
-                    //load drawerPage
-                    
-                }
-    
-                if(listeguesser != null && listeguesser.length <= 3){
-                    // server submits the 'guesser' event to this user
-                    console.log(listeguesser + ' are a guesser');
-                    listeguesser.forEach(function(){
-                        //load guesserPage
-                    });
-                } 
-            }
-                                  
-    
-            // Update current users
-            function updateConnectedUsers(){
-            nicknames = [];
-            for (i=0 ; i<users.length ; i++){
-                nicknames.push(users[i].clientName);
-              }
-            }
-
-            socket.on('draw', function(obj) {
-                socket.broadcast.emit('draw', obj);
-            });
-    
-            // submit each client's guesses to all clients
-            socket.on('guessword', function(data) {
-                server.emit('guessword', { username: data.username, guessword: data.guessword})
-                console.log('guessword event triggered on server from: ' + data.username + ' with word: ' + data.guessword);
-            });
-
-            socket.on('disconnect', function() {
-                for (var i = 0; i < users.length; i++) {
-        
-                    // remove user from users list
-                    if (users[i] == socket.username) {
-                        users.splice(i, 1);
-                    };
-                };
-                console.log(socket.username + ' has disconnected.');
-        
-                // submit updated users list to all clients
+            io.on('connection', function (socket) {
                 io.emit('userlist', users);
-        
-                // if 'drawer' room has no connections..
-                if ( typeof io.sockets.adapter.rooms['drawer'] === "undefined") {
-                    
-                    // generate random number based on length of users list
-                    var x = Math.floor(Math.random() * (users.length));
-                    console.log(users[x]);
-        
-                    // submit new drawer event to the random user in userslist
-                    io.in(users[x]).emit('new drawer', users[x]);
-                };
-            });
-        
-            socket.on('new drawer', function(name) {
-        
-                // remove user from 'guesser' room
-                socket.leave('guesser');
-        
-                // place user into 'drawer' room
-                socket.join('drawer');
-                console.log('new drawer emit: ' + name);
-        
-                // submit 'drawer' event to the same user
-                socket.emit('drawer', name);
+            
+                socket.on('join', function(name) {
+                    socket.username = name;
+            
+                    // user automatically joins a room under their own name
+                    socket.join(name);
+                    console.log(socket.username + ' has joined. ID: ' + socket.id);
+            
+                    // save the name of the user to an array called users
+                    users.push(socket.username);
+            
+                    // if the user is first to join OR 'drawer' room has no connections
+                    if (users.length == 1 || typeof io.sockets.adapter.rooms['drawer'] === 'undefined') {
+            
+                        // place user into 'drawer' room
+                        socket.join('drawer');
+            
+                        // server submits the 'drawer' event to this user
+                        io.in(socket.username).emit('drawer', socket.username);
+                        console.log(socket.username + ' is a drawer');
+            
+                        // send the random word to the user inside the 'drawer' room
+                        io.in(socket.username).emit('draw word', newWord());
+                    //	console.log(socket.username + "'s draw word (join event): " + newWord());
+                    } 
+            
+                    // if there are more than one names in users 
+                    // or there is a person in drawer room..
+                    else {
+            
+                        // additional users will join the 'guesser' room
+                        socket.join('guesser');
+            
+                        // server submits the 'guesser' event to this user
+                        io.in(socket.username).emit('guesser', socket.username);
+                        console.log(socket.username + ' is a guesser');
+                    }
                 
-                // send a random word to the user connected to 'drawer' room
-                io.in('drawer').emit('draw word', newWord());
+                    // update all clients with the list of users
+                    io.emit('userlist', users);
+                    
+                });
             
-            });
-        
-            // initiated from drawer's 'dblclick' event in Player list
-            socket.on('swap rooms', function(data) {
-        
-                // drawer leaves 'drawer' room and joins 'guesser' room
-                socket.leave('drawer');
-                socket.join('guesser');
-        
-                // submit 'guesser' event to this user
-                socket.emit('guesser', socket.username);
-        
-                // submit 'drawer' event to the name of user that was doubleclicked
-                io.in(data.to).emit('drawer', data.to);
-        
-                // submit random word to new user drawer
-                io.in(data.to).emit('draw word', newWord());
+                // submit drawing on canvas to other clients
+                socket.on('draw', function(obj) {
+                    socket.broadcast.emit('draw', obj);
+                });
             
-                io.emit('reset', data.to);
-        
+                // submit each client's guesses to all clients
+                socket.on('guessword', function(data) {
+                    io.emit('guessword', { username: data.username, guessword: data.guessword})
+                    console.log('guessword event triggered on server from: ' + data.username + ' with word: ' + data.guessword);
+                });
+            
+                socket.on('disconnect', function() {
+                    for (var i = 0; i < users.length; i++) {
+            
+                        // remove user from users list
+                        if (users[i] == socket.username) {
+                            users.splice(i, 1);
+                        };
+                    };
+                    console.log(socket.username + ' has disconnected.');
+            
+                    // submit updated users list to all clients
+                    io.emit('userlist', users);
+            
+                    // if 'drawer' room has no connections..
+                    if ( typeof io.sockets.adapter.rooms['drawer'] === "undefined") {
+                        
+                        // generate random number based on length of users list
+                        var x = Math.floor(Math.random() * (users.length));
+                        console.log(users[x]);
+            
+                        // submit new drawer event to the random user in userslist
+                        io.in(users[x]).emit('new drawer', users[x]);
+                    };
+                });
+            
+                socket.on('new drawer', function(name) {
+            
+                    // remove user from 'guesser' room
+                    socket.leave('guesser');
+            
+                    // place user into 'drawer' room
+                    socket.join('drawer');
+                    console.log('new drawer emit: ' + name);
+            
+                    // submit 'drawer' event to the same user
+                    socket.emit('drawer', name);
+                    
+                    // send a random word to the user connected to 'drawer' room
+                    io.in('drawer').emit('draw word', newWord());
+                
+                });
+            
+                // initiated from drawer's 'dblclick' event in Player list
+                socket.on('swap rooms', function(data) {
+            
+                    // drawer leaves 'drawer' room and joins 'guesser' room
+                    socket.leave('drawer');
+                    socket.join('guesser');
+            
+                    // submit 'guesser' event to this user
+                    socket.emit('guesser', socket.username);
+            
+                    // submit 'drawer' event to the name of user that was doubleclicked
+                    io.in(data.to).emit('drawer', data.to);
+            
+                    // submit random word to new user drawer
+                    io.in(data.to).emit('draw word', newWord());
+                
+                    io.emit('reset', data.to);
+            
+                });
+            
+                socket.on('correct answer', function(data) {
+                    io.emit('correct answer', data);
+                    console.log(data.username + ' guessed correctly with ' + data.guessword);
+                });
+            
+                socket.on('clear screen', function(name) {
+                    io.emit('clear screen', name);
+                });
+            
+            })
+            
+            server.listen(process.env.PORT || 8080, function() {
+                console.log('Server started at http://localhost:8080');
             });
-        
-            socket.on('correct answer', function(data) {
-                io.emit('correct answer', data);
-                console.log(data.username + ' guessed correctly with ' + data.guessword);
-            });
-        
-            socket.on('clear screen', function(name) {
-                io.emit('clear screen', name);
-        });
-    
-            // When client sends data
-            redisSubscriber.on("message", function(channel, data) {
-                broadcastToSockets(channel, data);
-              });     
-        }
-    }     
-}
-
-// Assign a random channel to people opening the application
-app.get("/", (req, res) => {
-    res.redirect(`/${uuidv4()}`);
-});
-
-app.get("/:channel", (req, res, next) => {
-    res.sendFile(path.join(PUBLIC_FOLDER, "index.html"), {}, err => {
-        if (err) {
-            next(err);
-        }
-    });
-});
-
-app.use(express.static(PUBLIC_FOLDER));
-
-server.listen(PORT, () => {
-    console.log(`Server started on port ${server.address().port}`);
-});
